1a7748e980a75812e95669cddf376d4cef0d626a
Mark Lawrence <markl.nz70@gmail.com>
2018-05-28 16:20:18 -0700

    New app with PWA requirements in place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  implements OnInit {
+
+  constructor(private swUpdate: SwUpdate) {
+  }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('A new version available. Load new version?')) {
+            window.location.reload();
+        }
+      });
+    }
+  }
 }
